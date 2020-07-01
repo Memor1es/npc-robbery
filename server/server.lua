5632b75d14daf58f036e2537c9cdbd206f55013e
michaelloder <mploder1337@gmail.com>
2020-07-01 16:34:27 +0100

    user now robs items as well as money

--- server/server.lua
+++ server/server.lua
@@ -4,9 +4,17 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 ESX.RegisterServerCallback('esx_robnpc:giveMoney', function(source, callback)
     local xPlayer = ESX.GetPlayerFromId(source)
+    local item = getStolenItem()
+    if(item.Name == 'money')
+    then
     local money = math.random(Config.MinMoney, Config.MaxMoney)
     xPlayer.addMoney(money)
-    callback(money)
+    callback('$'..money)
+    else
+        xPlayer.addInventoryItem(item.Name,1) 
+        callback(item.Label)
+    end
+    
 end)
 
 
@@ -29,3 +37,14 @@ RegisterServerEvent('civRobberyInProgress')
 AddEventHandler('civRobberyInProgress', function(street1, street2, sex)
 	TriggerClientEvent("civRobOutlawNotify", -1, "~r~Pedestrian robbery taking place by ~w~"..sex.." ~r~ between ~w~"..street1.."~r~ and ~w~"..street2)
 end)
+
+function getStolenItem()
+    local chanceNumber = math.random(Config.StolenItems.MinChance, Config.StolenItems.MaxChance)
+    print("number " .. chanceNumber)
+        for i=1, #Config.StolenItems.Items, 1 do
+             if(Config.StolenItems.Items[i].ChanceRangeMin <= chanceNumber and Config.StolenItems.Items[i].ChanceRangeMax >= chanceNumber)
+            then
+            return Config.StolenItems.Items[i];
+            end
+    end
+end
